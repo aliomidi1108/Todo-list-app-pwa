7bf6d75f4c72f484a259ba2f457ca425a7071dd0
aliomidi1108 <en.aliomidi@gmail.com>
2025-04-11 15:34:09 +0330

    تنظیم VitePWA و manifest برای GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,55 +3,56 @@ import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/Todo-list-app-pwa/',
   plugins: [
     tailwindcss(),
     VitePWA({
       manifest: {
         name: 'To-Do List',
         short_name: 'ToDo',
-        start_url: '/',
+        start_url: '/Todo-list-app-pwa/',
+        scope: '/Todo-list-app-pwa/',
         display: 'standalone',
         description: 'یه اپلیکیشن To-Do List ساده و زیبا - طراحی‌شده توسط Aliomidi',
         orientation: 'portrait',
         background_color: '#1E3A8A',
         theme_color: '#7DD3FC',
-        scope: '/',
         icons: [
           {
-            src: '/list72.png', 
+            src: 'list72.png',
             sizes: '72x72',
             type: 'image/png',
           },
           {
-            src: '/list128.png', 
+            src: 'list128.png',
             sizes: '128x128',
             type: 'image/png',
           },
           {
-            src: '/list144.png', 
+            src: 'list144.png',
             sizes: '144x144',
             type: 'image/png',
           },
           {
-            src: '/list192.png', 
+            src: 'list192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/list512.png', 
+            src: 'list512.png',
             sizes: '512x512',
             type: 'image/png',
           },
         ],
         screenshots: [
           {
-            src: '/screenshot1.png',
+            src: 'screenshot1.png',
             sizes: '540x720',
             type: 'image/png',
             form_factor: 'narrow',
           },
           {
-            src: '/screenshot2.png', 
+            src: 'screenshot2.png',
             sizes: '1280x800',
             type: 'image/png',
             form_factor: 'wide',
@@ -59,10 +60,6 @@ export default defineConfig({
         ],
       },
       registerType: 'autoUpdate',
-      workbox: {
-        
-      },
     }),
   ],
-  base: '/Todo-list-app-pwa/', 
 });
