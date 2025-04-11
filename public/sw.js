10ab4217b5f90cad85e2e3a1452545f1289f237b
aliomidi1108 <en.aliomidi@gmail.com>
2025-04-11 14:58:43 +0330

    به‌روزرسانی index.html، manifest و sw.js برای PWA

--- public/sw.js
+++ public/sw.js
@@ -10,4 +10,24 @@ self.addEventListener('install', (event) => {
   //self.addEventListener('fetch', (event) => {
     // می‌تونی بعداً کش هم اضافه کنی
  // });
-  
+  // sw.js
+const CACHE_NAME = 'todo-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/Todo-list-app-pwa/'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
